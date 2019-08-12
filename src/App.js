60094df40c89a311fb11bb2625486601696165f9
ml250271 <lukicmilica@hotmail.com>
2019-08-13 00:11:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Movies from "./components/Movies";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <main className="container">
+                <Movies />
+            </main>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Movies.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/Like";
+import Pagination from "./common/Pagination";
+
+class Movies extends Component {
+    state = { movies: getMovies(), pageSize: 4, currentPage: 1 };
+
+    handleDelete = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({ movies });
+    };
+
+    handleLike = movie => {
+        const movies = [...this.state.movies];
+        const index = movies.indexOf(movie);
+        movies[index] = { ...movies[index] };
+        movies[index].liked = !movies[index].liked;
+        this.setState({ movies });
+    };
+
+    handlePageChange = page => {
+        this.setState({ currentPage: page });
+    };
+
+    render() {
+        const { pageSize, currentPage } = this.state;
+        const count = this.state.movies.length;
+        if (count === 0) {
+            return <p>No movies</p>;
+        }
+        return (
+            <>
+                <p>Showing {count} movies.</p>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Rate</th>
+                            <th />
+                            <th />
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.state.movies.map(movie => {
+                            return (
+                                <tr key={movie._id}>
+                                    <td>{movie.title}</td>
+                                    <td>{movie.genre.name}</td>
+                                    <td>{movie.numberInStock}</td>
+                                    <td>{movie.dailyRentalRate}</td>
+                                    <td>
+                                        <Like
+                                            liked={movie.liked}
+                                            onLike={() =>
+                                                this.handleLike(movie)
+                                            }
+                                        />
+                                    </td>
+                                    <td>
+                                        <button
+                                            onClick={() =>
+                                                this.handleDelete(movie)
+                                            }
+                                            className="btn btn-danger btn-sm"
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+                <Pagination
+                    pageSize={pageSize}
+                    itemsCount={count}
+                    currentPage={currentPage}
+                    onPageChange={this.handlePageChange}
+                />
+            </>
+        );
+    }
+}
+
+export default Movies;
